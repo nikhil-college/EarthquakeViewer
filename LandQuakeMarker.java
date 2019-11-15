a526bfca161f50cf99ceee36e8e9f7c7f1cb554d
nikhil-college <45045009+nikhil-college@users.noreply.github.com>
2019-11-15 17:27:17 +0530

    Update LandQuakeMarker.java

--- LandQuakeMarker.java
+++ LandQuakeMarker.java
@@ -3,11 +3,7 @@ package module6;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import processing.core.PGraphics;
 
-/** Implements a visual marker for land earthquakes on an earthquake map
- * 
- * @author UC San Diego Intermediate Software Development MOOC team
- *
- */
+
 public class LandQuakeMarker extends EarthquakeMarker {
 	
 	
@@ -23,12 +19,7 @@ public class LandQuakeMarker extends EarthquakeMarker {
 
 	@Override
 	public void drawEarthquake(PGraphics pg, float x, float y) {
-		// IMPLEMENT: drawing circle for LandQuake
-		// DO NOT set the fill color.  That will be set in the EarthquakeMarker
-		// class to indicate the depth of the earthquake.
-		// Simply draw a centered square.
-		// HINT: Notice the radius variable in the EarthquakeMarker class
-		// and how it is set in the EarthquakeMarker constructor
+		
 		pg.ellipse(x, y, 2*radius, 2*radius);
 		
 	}
